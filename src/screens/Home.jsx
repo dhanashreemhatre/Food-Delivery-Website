eea02e55ffc7eb2ec8f27b4ee15bc8836e25048b
Dhanashree <mhatredhanashree682@gmail.com>
2024-04-22 21:54:25 +0530

    Initial commit

---
+++ src/screens/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+function Home() {
+  return (
+    <div>
+        <div><Navbar/></div>
+        <div>Body</div>
+        <div><Footer/></div>
+    </div>
+  )
+}
+
+export default Home
